7422f4bf033bc081f1ef7fac7feca6ac9a0c9c7f
youngerous <youngerous@gmail.com>
2021-03-18 23:00:38 +0900

    update

--- run_kobart.sh
+++ run_kobart.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-BATCH_SIZE=8
+BATCH_SIZE=10
 EPOCH=5
-
+LR=3e-5
 
 python src/kobart/main.py\
     --batch-size=${BATCH_SIZE}\
+    --lr=${LR}\
     --epoch=${EPOCH}\
-    --amp\
     --distributed\
+
